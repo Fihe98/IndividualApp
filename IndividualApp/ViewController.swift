add4c8cc143f51451352b6980cbdc8e25e8d8e0e
FiHe98 <henriksson.filip1998@gmail.com>
2024-06-10 12:55:52 +0200

    Initial Commit

---
+++ IndividualApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IndividualApp
+//
+//  Created by Filip Henriksson on 2024-06-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
